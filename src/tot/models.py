c80435ef23f6abd1dc2683488af574f9f1374f35
travisnp <tnpence@email.sc.edu>
2024-12-11 13:53:59 -0600

    bug

--- src/tot/models.py
+++ src/tot/models.py
@@ -27,8 +27,8 @@ def chatgpt(messages, model="llama", temperature=0.7, max_tokens=1000, n=1, stop
         cnt = min(n, 20)
         n -= cnt
         res = completions_with_backoff(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, n=cnt, stop=stop)
-        print("res: ", res)
         outputs.append(res[0]["generated_text"][0]["content"])
+        print("res: ", outputs[-1])
         # log completion tokens
     print("Outputs: ", outputs)
     return outputs
